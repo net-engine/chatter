33215d36a518ca95e47f132ec132e0e66a4d265f
Dan Sowter <dansowter@me.com>
2012-07-02 22:31:16 +1000

    Shows messages, adds simplecov

--- spec/requests/logins_spec.rb
+++ spec/requests/logins_spec.rb
@@ -15,14 +15,23 @@ describe "Guests" do
 end
 
 describe "Signed in users" do
-  it "see their email address" do
-    FactoryGirl.create(:user, email: "demo@example.com", password: "demo123")
-
+  before(:each) do
+    @user = FactoryGirl.create(:user, email: "demo@example.com", password: "demo123")
     visit sign_in_path
     fill_in 'Email', with: "demo@example.com"
     fill_in 'Password', with: "demo123"
     click_button 'Sign in'
+  end
 
+  it "see their email address" do
+    visit root_path
     page.should have_content("demo@example.com")
+  end
+
+  it "see their messages" do
+    @user.messages.create(content: "Here's a lovely message.")
+
+    visit root_path
+    page.should have_content("Here's a lovely message.")
   end
 end
